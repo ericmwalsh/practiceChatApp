ccedf04de4185e3a8d2a7bd9be654daf83b66b5d
Eric Walsh <ericwalsh@Erics-MacBook-Air-2.local>
2014-11-20 14:37:40 -0800

    Initial commit, barebones

---
+++ public/javascripts/client.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+
+	var name = '';
+	function go() {
+		name = $('#user-name').val();
+		$('#user-name').val('');
+		$('.user-form').hide();
+		$('.chat-box').show();
+		console.log(name);
+	};
+
+	$('#user-name').keydown(function(e){
+		if(e.keyCode == 13){ //Enter pressed
+			go();
+		}
+	});
+	$('.go-user').on('click', function(e) {
+		go();
+	});
+	$('.chat-box textarea').keydown(function(e) {
+		if(e.keyCode == 13){
+			$.ajax({
+				type: "POST",
+				url: "/msg",
+				data: JSON.stringify({"username" : name, "message" : $('#message-input').val().trim()}),
+				contentType: "application/json"
+			});
+			$(this).val('');
+			$('.jumbotron').hide();
+			e.preventDefault();
+		}
+	});
+
+ });
